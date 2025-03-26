3d24a5275d6d059cea3a44749fdeeff0a913d4cc
LOUDO <lucaslelouer@gmail.com>
2025-03-26 13:26:26 +0100

    Fixed: Dropper help command message

--- src/main/java/fr/loudo/dropperReloaded/commands/dropper/CommandHelpPlayer.java
+++ src/main/java/fr/loudo/dropperReloaded/commands/dropper/CommandHelpPlayer.java
@@ -10,9 +10,9 @@ public class CommandHelpPlayer {
 
         StringBuilder helpMessage = new StringBuilder();
 
-        helpMessage.append(prefixCmd).append("map play").append(separator).append("Join a game.\n")
-                .append(prefixCmd).append("map leave").append(separator).append("Leave your current game.\n")
-                .append(prefixCmd).append("map stats").append(separator).append("Show your stats.\n");
+        helpMessage.append(prefixCmd).append("play").append(separator).append("Join a game.\n")
+                .append(prefixCmd).append("leave").append(separator).append("Leave your current game.\n")
+                .append(prefixCmd).append("stats").append(separator).append("Show your stats.\n");
 
 
         return helpMessage.toString();
